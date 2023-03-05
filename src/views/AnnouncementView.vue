2fb78280df6fb2fd022332fbe3cf50789db29998
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-03-05 16:09:05 +0900

    feat: AnnouncementItem click 이벤트 핸들링 임시 구현

--- src/views/AnnouncementView.vue
+++ src/views/AnnouncementView.vue
@@ -1,6 +1,11 @@
 <script setup>
 import SearchBar from '../components/SearchBar.vue'
 import AnnouncementItem from '../components/AnnouncementItem.vue'
+
+function itemClick(id) {
+  // TODO: 공지사항 내용을 띄우는 코드 작성하기
+  console.log(`announcement item ${id} clicked`)
+}
 </script>
 
 <template>
@@ -9,14 +14,16 @@ import AnnouncementItem from '../components/AnnouncementItem.vue'
     <div class="search-bar"><SearchBar /></div>
     <div class="announcement-list">
       <AnnouncementItem
+        @click="() => itemClick(1)"
         title="축제 진행에 대한 공지"
         content="안녕하세요. 2023 한림대학교 비봉축전 준비 위원회 공지사항입니다. 감사합니다."
       />
       <AnnouncementItem
+        @click="() => itemClick(2)"
         title="Lorem Ipsum"
         content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       />
-      <AnnouncementItem title="멋쟁이사자처럼" content="123123123123" />
+      <AnnouncementItem @click="() => itemClick(3)" title="멋쟁이사자처럼" content="123123123123" />
     </div>
   </main>
 </template>
