d4f4f04aac96fad28435af819a90f40c482cf70d
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-03-19 18:14:31 +0900

    chore: osohyun0224 - 20230316 코드 병합

---
+++ src/components/MyModal.vue
@@ -0,0 +1,74 @@
+<template>
+  <transition name="modal" appear>
+    <div class="modal modal-overlay" @click.self="$emit('close')">
+      <div class="modal-window">
+        <div class="modal-content">
+          <slot/>
+        </div>
+        <footer class="modal-footer">
+          <slot name="footer">
+            <button @click="$emit('close')">닫기</button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+
+.modal-footer {
+  background: #ccc;
+  padding: 10px;
+  text-align: right;
+}
+.modal-content {
+  padding: 10px 20px;
+}
+
+.modal-window {
+  background: #fff;
+  border-radius: 4px;
+  overflow: hidden;
+}
+
+.modal.modal-overlay {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  z-index: 30;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.modal-enter-active, .modal-leave-active {
+  /* 오버레이 트랜지션 */
+  transition: opacity 0.4s;
+
+ 
+
+}
+
+.modal-window {
+  transition: opacity 0.4s, transform 0.4s;
+}
+
+.modal-leave-active {
+  transition: opacity 0.6s ease 0.4s;
+}
+
+.modal-enter, .modal-leave-to {
+  opacity: 0;
+
+
+}
+
+.modal-window {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+</style>
--- src/views/BoothView.vue
+++ src/views/BoothView.vue
@@ -5,27 +5,118 @@
       <img :src="data.image || 'https://via.placeholder.com/700x400/D9D9D9/aaaaaa'" />
     </div>
 
-    <h1>부스 소개</h1>
+    <div class="edit">
+      <h1>부스 소개</h1>
+      <div class="example-modal-window">
+        <button @click="openModal" type="Edbutton">
+          <img src="@/assets/edit_button.png" alt="" />
+        </button>
+
+        <!-- 컴포넌트 MyModal -->
+        <MyModal @close="closeModal" v-if="modal">
+          <!-- default 슬롯 콘텐츠 -->
+          <p>관리자 내용 수정 창</p>
+          <div><input v-model="message" /></div>
+          <!-- /default -->
+          <!-- footer 슬롯 콘텐츠 -->
+          <template slot="footer">
+            <button @click="doSend">수정하기</button>
+          </template>
+          <!-- /footer -->
+        </MyModal>
+      </div>
+    </div>
+
+    <hr class="hr-solid" />
     <p v-text="data.mainDescription || 'Loading...'"></p>
 
-    <h1>메뉴 소개</h1>
+    <div class="edit">
+      <h1>메뉴 소개</h1>
+      <button type="Edbutton"><img src="@/assets/edit_button.png" alt="" /></button>
+    </div>
+    <hr class="hr-solid" />
+    <p v-text="data.menuDescription || 'Loading...'"></p>
+
+    <h2>댓글</h2>
+    <hr class="hr-solid" />
     <p v-text="data.menuDescription || 'Loading...'"></p>
 
     <div class="button">
       <button @click="() => $router.push('/boothmap')">뒤로 돌아가기</button>
     </div>
   </main>
+  <div>
+    <h1 v-text="data.name || 'Loading...'"></h1>
+    <div class="image">
+      <img :src="data.image || 'https://via.placeholder.com/700x400/D9D9D9/aaaaaa'" />
+    </div>
+
+    <div class="edit">
+      <h1>부스 소개</h1>
+      <button type="Edbutton"><img src="@/assets/edit_button.png" alt="" /></button>
+    </div>
+    <hr class="hr-solid" />
+    <p v-text="data.mainDescription || 'Loading...'"></p>
+
+    <div class="edit">
+      <h1>메뉴 소개</h1>
+      <button type="Edbutton"><img src="@/assets/edit_button.png" alt="" /></button>
+    </div>
+    <hr class="hr-solid" />
+    <p v-text="data.menuDescription || 'Loading...'"></p>
+
+    <h2>댓글</h2>
+    <hr class="hr-solid" />
+    <div class="nickname">
+      <p>
+        닉네임: <input class="user-name" type="nickname" @input="userId = $event.target.value" />
+      </p>
+    </div>
+    <div class="password">
+      <p>
+        비밀번호:
+        <input class="user-pw" type="pw" maxlength="4" @input="userId = $event.target.value" />
+      </p>
+    </div>
+    <div class="login_button">
+      <button type="Edbutton"><img src="@/assets/edit_button.png" alt="" /></button>
+    </div>
+
+    <div class="button">
+      <button @click="() => $router.push('/boothmap')">뒤로 돌아가기</button>
+    </div>
+  </div>
 </template>
 
 <script>
 import { GetDemoBooth } from '../api/api-client';
+import MyModal from '../components/MyModal.vue';
 
 export default {
-  components: {},
+  components: { MyModal },
   data() {
     return {
-      data: {}
+      data: {},
+      modal: false,
+      message: ''
     };
+  },
+  methods: {
+    openModal() {
+      this.modal = true;
+    },
+    closeModal() {
+      this.modal = false;
+    },
+    doSend() {
+      if (this.message.length > 0) {
+        alert(this.message);
+        this.message = '';
+        this.closeModal();
+      } else {
+        alert('내용을 수정해주세요.');
+      }
+    }
   },
   created() {
     // 데이터 가져오기
@@ -43,13 +134,22 @@ export default {
 </script>
 
 <style scoped>
+.hr-solid {
+  border: 0px;
+  border-top: 3px solid #000000;
+}
 h1 {
   font-size: 20pt;
-  text-align: center;
+  text-align: left;
   margin: 0;
-  padding: 24px 0;
 }
-
+.edit {
+  font-size: 20pt;
+  text-align: left;
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+}
 button {
   padding: 0;
   border: none;
