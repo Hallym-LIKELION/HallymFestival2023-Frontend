46f435361588c69451ff1133a83cd20ffaa8bde7
osohyun0224 <osohyun0224@nvaer,com>
2023-03-21 21:11:45 +0900

    animation of scroll

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import AOS from 'aos';
+import "aos/dist/aos.css";
 
 import './assets/main.css';
 
@@ -9,3 +11,12 @@ const app = createApp(App);
 app.use(router);
 
 app.mount('#app');
+
+new Vue({
+  created() {
+      AOS.init();
+  },
+el: '#app',
+router,
+render: h => h(App)
+})
