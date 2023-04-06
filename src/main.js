46ed5811ae23dd5b07ff5d50ed41af4cd127e928
osohyun0224 <osohyun0224@naver.com>
2023-04-06 15:41:08 +0900

    fix: 페이지네이션 오류 수정 최종

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { PaginationControl, PaginationPage } from 'vue-smart-pagination';
 
 import 'aos/dist/aos.css';
 
 import './assets/main.css';
 
-
-Vue.component('PaginationControl', PaginationControl);
-Vue.component('PaginationPage', PaginationPage);
 const app = createApp(App);
 
 app.use(router);
