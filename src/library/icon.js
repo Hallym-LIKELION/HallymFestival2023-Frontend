eb744b11d022625d307b3e6d55bd674435710ac4
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-04-11 17:06:32 +0900

    feat: plus 아이콘 추가

--- src/library/icon.js
+++ src/library/icon.js
@@ -2,8 +2,11 @@
 
 import leftArrow from '../assets/icon/left-arrow.svg';
 import rightArrow from '../assets/icon/right-arrow.svg';
+import plus from '../assets/icon/plus.svg';
+// import  from '../assets/icon/';
 
 export const Icon = {
   leftArrow,
-  rightArrow
+  rightArrow,
+  plus
 };
