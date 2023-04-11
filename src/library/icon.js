0ce7f0f936f3964b09a0d6665efa55b5edd7dd2b
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-04-11 17:26:44 +0900

    feat: sun, moon 아이콘 추가

--- src/library/icon.js
+++ src/library/icon.js
@@ -3,10 +3,14 @@
 import leftArrow from '../assets/icon/left-arrow.svg';
 import rightArrow from '../assets/icon/right-arrow.svg';
 import plus from '../assets/icon/plus.svg';
+import sun from '../assets/icon/sun.svg';
+import moon from '../assets/icon/moon.svg';
 // import  from '../assets/icon/';
 
 export const Icon = {
   leftArrow,
   rightArrow,
-  plus
+  plus,
+  sun,
+  moon
 };
