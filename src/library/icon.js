f769db3c080665727c40fec1ab7d3e68c91580d5
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-04-11 16:26:16 +0900

    feat: icon들을 라이브러리에 담아서 사용하도록 수정

---
+++ src/library/icon.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import leftArrow from '../assets/icon/left-arrow.svg';
+import rightArrow from '../assets/icon/right-arrow.svg';
+
+export const Icon = {
+  leftArrow,
+  rightArrow
+};
