30ac0bdcdbde2f9c7cf4e02178e92fc2bd49a967
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-04-12 22:28:32 +0900

    feat: 부스 유형 아이콘 추가

--- src/library/icon.js
+++ src/library/icon.js
@@ -5,6 +5,10 @@ import rightArrow from '../assets/icon/right-arrow.svg';
 import plus from '../assets/icon/plus.svg';
 import sun from '../assets/icon/sun.svg';
 import moon from '../assets/icon/moon.svg';
+import beer from '../assets/icon/beer.svg';
+import basket from '../assets/icon/basket.svg';
+import food from '../assets/icon/food.svg';
+import gamepad from '../assets/icon/gamepad.svg';
 // import  from '../assets/icon/';
 
 export const Icon = {
@@ -12,5 +16,9 @@ export const Icon = {
   rightArrow,
   plus,
   sun,
-  moon
+  moon,
+  beer,
+  basket,
+  food,
+  gamepad
 };
