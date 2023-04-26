5275d0ba127ffb4af94ce043ab6104f86186f084
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-04-26 18:38:22 +0900

    chore: vue3-carousel 패키지 추가

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import VueAwesomePaginate from 'vue-awesome-paginate';
 // 축제 공용 CSS import
 import './assets/main.css';
 
+import 'vue3-carousel/dist/carousel.css'; // Carousel 패키지 CSS
 import 'vue-awesome-paginate/dist/style.css'; // Pagenation 패키지 CSS
 import 'aos/dist/aos.css'; // AOS 패키지 CSS
 
