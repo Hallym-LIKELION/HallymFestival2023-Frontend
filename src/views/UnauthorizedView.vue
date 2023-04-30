3ee3e62a9a23871b2283ba55b2362d0b8fc2d6d6
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-04-30 23:35:08 +0900

    feat: 401 error 페이지 추가

---
+++ src/views/UnauthorizedView.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <h1>401 Unauthorized</h1>
+    <p>해당 페이지에 접속하려면 최고 관리자 권한이 필요합니다.</p>
+    <div><RouterLink class="button" to="/">HOME으로 돌아가기</RouterLink></div>
+  </main>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+export default {
+  components: {
+    RouterLink
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20pt;
+  text-align: center;
+  padding: 36px 0;
+  color: white;
+}
+
+p {
+  text-align: center;
+  font-size: 16pt;
+  color: white;
+}
+
+div {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  color: white;
+}
+
+.button {
+  margin-top: 36px;
+  padding: 8px 16px;
+  border-radius: 24px;
+  background-color: #509bf8;
+  font-size: 16pt;
+  text-decoration: none;
+  color: white;
+}
+</style>
