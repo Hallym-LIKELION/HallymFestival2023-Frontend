f0acb9c7564517eede283570af9e8252510d0eb6
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-05-06 00:37:27 +0900

    feat: 랜덤 닉네임 IPv6 지원 추가

--- src/library/name-generator.js
+++ src/library/name-generator.js
@@ -5,7 +5,7 @@ import { noun, adjective } from './name.json';
 
 /**
  * 특정한 시드를 이용하여 난수를 생성하는 함수를 만듭니다.
- * Source: https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
+ * Source: https://stackoverflow.com/a/72732727
  * @param {Number} seed
  * @returns {() => number} seed를 시드로 하는 난수 생성 함수
  */
@@ -24,18 +24,39 @@ function GetRandomFunction(seed) {
  * @param {String} ip "0.0.0.0", "255.255.255.255" 와 같은 형식을 지닌 IP 문자열
  * @returns {Number} IPv4 문자열을 정수로 변환한 값입니다.
  */
-function IpToInt(ip) {
+function Ipv4ToInt(ip) {
   const list = ip.split('.');
-  return list.reduce((result, item, index) => result + item * Math.pow(256, 3 - index), 0);
+  return list.reduce((result, item, index) => result + item * 256 ** index, 0);
+}
+
+/**
+ * IPv6 문자열을 10진수 정수로 변환합니다.
+ * @param {String} ip "0:0:0:0:0:0:0:0", "2606:2800:0220:0001:0248:1893:25c8:1946" 와 같은 형식을 지닌 IP 문자열
+ * @returns {Number} IPv6 문자열을 정수로 변환한 값입니다.
+ */
+function Ipv6ToInt(ip) {
+  // 숫자가 너무 커서 BigInt를 사용
+  const list = ip.split(':').map((item) => parseInt(item, 16));
+  return list.reduce((result, item, index) => result + BigInt(item) * 65535n ** BigInt(index), 0n);
 }
 
 /**
  * 주어진 문자열을 시드로 사용하여 랜덤한 닉네임을 만듭니다.
- * @param {String} ip 닉네임을 만들 때 사용할 시드 ip 문자열입니다.
+ * @param {String} ip 닉네임을 만들 때 사용할 시드 ip 문자열입니다. (v4, v6 모두 지원)
  * @returns {String} 랜덤하게 생성된 닉네임입니다.
  */
 export function GetRandomNickName(ip) {
-  const seed = IpToInt(ip);
+  let seed = 0;
+  const IPv4 = /\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}/g;
+  const IPv6 =
+    /[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}/g;
+
+  if (IPv4.test(ip)) {
+    seed = Ipv4ToInt(ip);
+  } else if (IPv6.test(ip)) {
+    seed = Ipv6ToInt(ip);
+  }
+
   const GetRandom = GetRandomFunction(seed);
   const nounIndex = Math.floor(GetRandom() * noun.length);
   const adjIndex = Math.floor(GetRandom() * adjective.length);
