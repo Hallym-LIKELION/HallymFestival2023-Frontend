fc32c7c659bf5c2ff5eb8b50c31513483dcf45e9
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-05-07 16:21:06 +0900

    fix: 미사용 패키지 제거 2

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import './assets/main.css';
 
 import 'vue3-carousel/dist/carousel.css'; // Carousel 패키지 CSS
 import 'vue-awesome-paginate/dist/style.css'; // Pagenation 패키지 CSS
-import 'aos/dist/aos.css'; // AOS 패키지 CSS
 import 'vue-toastification/dist/index.css';
 
 const app = createApp(App);
