f736b64b87d34ef0ab1fb641229ef8ec7f205f3d
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-05-08 20:24:16 +0900

    feat: footer에 네이버 클라우드 링크 연결

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="footer">
     <p class="title">한림대학교 멋쟁이사자처럼 Builder Team</p>
-    <p class="sponser">
+    <p class="sponser" @click="openNcp">
       본 사이트는 Naver Cloud Platform의 <br />
       지원을 받아 사용하고 있습니다.
     </p>
     <p class="subtitle">Hallym Likelion</p>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    openNcp() {
+      window.open('https://www.ncloud.com/', '_blank');
+    }
+  }
+};
+</script>
+
 <style scoped>
 .footer {
   padding: 10px;
@@ -29,6 +40,7 @@
   margin-top: 2px;
   font-size: 8pt;
   color: #ffffff;
+  cursor: pointer;
 }
 .footer > p.subtitle {
   margin-top: 8px;
