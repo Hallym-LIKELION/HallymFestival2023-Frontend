ae1b9d33b9a45bdd2eebb7f363a4d9e9ba99dd77
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-05-09 13:19:00 +0900

    feat: 부스 배치도 good byee

--- src/components/BoothCarousel.vue
+++ src/components/BoothCarousel.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
     <Carousel :items-to-show="1" :wrapAround="true" v-model="_slide">
-      <Slide v-if="isAdmin" v-for="(item, index) in charts" :key="index">
+      <Slide v-for="(item, index) in charts" :key="index">
         <Chart :options="item"></Chart>
-      </Slide>
-      <Slide v-if="!isAdmin" v-for="(item, index) in image" :key="index">
-        <Image class="image" :src="item" width="500" height="400" spinner-size="300" />
       </Slide>
 
       <template #addons>
@@ -18,7 +15,6 @@
 <script>
 import { Chart } from 'highcharts-vue';
 import { Carousel, Slide, Navigation } from 'vue3-carousel';
-import Image from '../components/Image.vue';
 import {
   GetBoothListWithComment,
   GetBoothListWithLike,
@@ -103,22 +99,11 @@ export default {
     return {
       _slide: 0,
 
-      charts: [],
-
-      image: [
-        'https://placehold.co/500x400/aa2222/FFFFFF/png?text=Tuesday',
-        'https://placehold.co/500x400/22AA22/FFFFFF/png?text=Wednesday',
-        'https://placehold.co/500x400/3333AA/FFFFFF/png?text=Thursday',
-        'https://placehold.co/500x400/222222/FFFFFF/png?text=Night+time'
-      ]
+      charts: []
     };
   },
 
   props: {
-    isAdmin: {
-      type: Boolean,
-      default: false
-    },
     slide: {
       type: Number,
       default: 0
@@ -136,25 +121,20 @@ export default {
   },
 
   async created() {
-    if (this.isAdmin) {
-      const commentList = (await GetBoothListWithComment()).dtoList;
-      const likeList = (await GetBoothListWithLike()).dtoList;
-      const reportList = (await GetBoothListWithReport()).dtoList;
+    const commentList = (await GetBoothListWithComment()).dtoList;
+    const likeList = (await GetBoothListWithLike()).dtoList;
+    const reportList = (await GetBoothListWithReport()).dtoList;
 
-      let data;
+    let data;
 
-      data = commentList.map((item) => [item.booth_title, item.comment_cnt]).slice(0, 5);
+    data = commentList.map((item) => [item.booth_title, item.comment_cnt]).slice(0, 5);
+    this.charts.push(CreateChart('댓글이 가장 많은 부스 TOP 5', '댓글', data));
 
-      this.charts.push(CreateChart('댓글이 가장 많은 부스 TOP 5', '댓글', data));
+    data = likeList.map((item) => [item.booth_title, item.like_cnt]).slice(0, 5);
+    this.charts.push(CreateChart('좋아요가 가장 많은 부스 TOP 5', '좋아요', data));
 
-      data = likeList.map((item) => [item.booth_title, item.like_cnt]).slice(0, 5);
-
-      this.charts.push(CreateChart('좋아요가 가장 많은 부스 TOP 5', '좋아요', data));
-
-      data = reportList.map((item) => [item.booth_title, item.report_cnt]).slice(0, 5);
-
-      this.charts.push(CreateChart('신고받은 댓글이 가장 많은 부스 TOP 5', '신고받은 댓글', data));
-    }
+    data = reportList.map((item) => [item.booth_title, item.report_cnt]).slice(0, 5);
+    this.charts.push(CreateChart('신고받은 댓글이 가장 많은 부스 TOP 5', '신고받은 댓글', data));
   }
 };
 </script>
